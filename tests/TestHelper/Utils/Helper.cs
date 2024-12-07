3056b8a56c70ab9d6a49e67c9998e0ffe639fc29
Kuroki-g <64629990+Kuroki-g@users.noreply.github.com>
2024-12-07 13:44:31 +0900

    Add TestHelper package.

---
+++ tests/TestHelper/Utils/Helper.cs
@@ -0,0 +1,32 @@
+﻿namespace TestHelper.Utils;
+
+public static class Helper
+{
+    /// <summary>
+    /// Clean up specific file of target directory.
+    /// </summary>
+    /// <param name="dbDirectory"></param>
+    /// <param name="fileName"></param>
+    public static void CleanUp(string dbDirectory, string fileName)
+    {
+        // Clean up
+        var info = new FileInfo(Path.Combine(dbDirectory, fileName));
+        if (info.Exists)
+            info.Delete();
+    }
+
+    /// <summary>
+    /// Clean up target directory.
+    /// </summary>
+    /// <param name="dbDirectory"></param>
+    /// <param name="fileName"></param>
+    public static void CleanUp(string dbDirectory)
+    {
+        // Clean up
+        var dir = new DirectoryInfo(dbDirectory);
+        foreach (var info in dir.GetFiles())
+        {
+            info.Delete();
+        }
+    }
+}
