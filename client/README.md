162bfbc467bde8379cddd2789f66b90f91435d6c
Kuroki-g <64629990+Kuroki-g@users.noreply.github.com>
2024-12-31 00:14:25 +0900

    Add SimpleDB.NET client project with initial implementation and README

---
+++ client/README.md
@@ -0,0 +1,3 @@
+# SimpleDB.NET Client sample
+
+This is a simple example of how to use the SimpleDB.NET client library.
---
+++ src/SimpleDB.Provider/SqlClient.cs
@@ -0,0 +1,23 @@
+﻿namespace SimpleDB.Provider;
+
+using Grpc.Net.Client;
+using GrpcGreeter;
+
+public class SqlClient
+{
+    public string GetMessage() => "Hello from SimpleDB.Provider";
+
+    public void Sample()
+    {
+        var handler = new HttpClientHandler();
+        handler.ServerCertificateCustomValidationCallback =
+            HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+        // The port number must match the port of the gRPC server.
+        using var channel = GrpcChannel.ForAddress(
+            "https://localhost:7279",
+            new GrpcChannelOptions { HttpHandler = handler }
+        );
+        var client = new Greeter.GreeterClient(channel);
+    }
+}
