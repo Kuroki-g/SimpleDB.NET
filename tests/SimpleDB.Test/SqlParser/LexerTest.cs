36e38d871d9a64c56ce80a22da2a21a4f4fd7dff
Kuroki-g <64629990+Kuroki-g@users.noreply.github.com>
2025-01-05 17:04:45 +0900

    Refactor Lexer tests to improve clarity and add Parser tests for future implementation

--- tests/SimpleDB.Test/SqlParser/LexerTest.cs
+++ tests/SimpleDB.Test/SqlParser/LexerTest.cs
@@ -4,23 +4,46 @@ namespace SimpleDB.Test.SqlParser;
 
 public class LexerTest
 {
+    // [Theory]
+    // [InlineData("invalid-word")]
+    // public void LexerTest_EatIdentifier_throws_exception(string input)
+    // {
+    //     var lex = new Lexer(input);
+
+    //     var actual = Record.Exception(() => lex.EatIdentifier());
+
+    //     Assert.IsType<BadSyntaxException>(actual);
+    // }
+
     [Fact]
-    public void LexerTest_new()
+    public void EatIntConstant()
     {
-        var lex = new Lexer("");
-        string x;
-        int y;
-        if (lex.IsIdMatch)
-        {
-            x = lex.EatId();
-            lex.EatDelim('=');
-            y = lex.EatIntConstant();
-        }
-        else
-        {
-            y = lex.EatIntConstant();
-            lex.EatDelim('=');
-            x = lex.EatId();
-        }
+        var lex = new Lexer("1");
+
+        var actual = lex.EatIntConstant();
+
+        Assert.Equal(1, actual);
+    }
+
+    [Theory]
+    [InlineData("'1'")]
+    [InlineData("\"1\"")]
+    public void EatStringConstant(string input)
+    {
+        var lex = new Lexer(input);
+
+        var actual = lex.EatStringConstant();
+
+        Assert.Equal("1", actual);
+    }
+
+    [Fact]
+    public void MatchKeyword_assert()
+    {
+        var lex = new Lexer("select * from table where a = 1");
+
+        var actual = lex.MatchKeyword("select");
+
+        Assert.True(actual);
     }
 }
---
+++ tests/SimpleDB.Test/SqlParser/ParserTest.cs
@@ -0,0 +1,14 @@
+using SimpleDB.SqlParser;
+
+namespace SimpleDB.Test.SqlParser;
+
+public class ParserTest
+{
+    // [Fact]
+    // public void Test1()
+    // {
+    //     var s = "select ;";
+    //     var p = new Parser(s);
+    //     p.Query();
+    // }
+}
