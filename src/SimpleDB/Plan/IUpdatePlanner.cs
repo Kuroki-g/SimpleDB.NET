8e4257711d1eb363f0479a0609336ba0c62de139
Kuroki-g <64629990+Kuroki-g@users.noreply.github.com>
2025-01-13 20:11:16 +0900

    Add IUpdatePlanner interface for managing update operations in query plans

---
+++ src/SimpleDB/Plan/IUpdatePlanner.cs
@@ -0,0 +1,20 @@
+using SimpleDB.SqlParser;
+using SimpleDB.SqlParser.Grammar;
+using SimpleDB.Tx;
+
+namespace SimpleDB.Plan;
+
+public interface IUpdatePlanner
+{
+    // public int ExecuteModify(Update cmd, ITransaction tx);
+
+    public int ExecuteInsert(Insert cmd, ITransaction tx);
+
+    public int ExecuteDelete(Delete cmd, ITransaction tx);
+
+    public int ExecuteCreateTable(CreateTable cmd, ITransaction tx);
+
+    // public int ExecuteCreateView(CreateView cmd, ITransaction tx);
+
+    // public int ExecuteCreateIndex(CreateIndex cmd, ITransaction tx);
+}
