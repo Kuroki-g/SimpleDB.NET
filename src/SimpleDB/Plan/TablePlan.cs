5c3f309dc325ff9428bb4fa8f820b9c41b1d2459
Kuroki-g <64629990+Kuroki-g@users.noreply.github.com>
2025-01-13 16:59:04 +0900

    Add TablePlan class for managing table metadata and scan operations

---
+++ src/SimpleDB/Plan/TablePlan.cs
@@ -0,0 +1,41 @@
+using SimpleDB.Metadata;
+using SimpleDB.Sql;
+using SimpleDB.Structure;
+using SimpleDB.Tx;
+
+namespace SimpleDB.Plan;
+
+public class TablePlan
+{
+    private readonly string _tableName;
+
+    private readonly ITransaction _tx;
+
+    private readonly Layout _layout;
+
+    private readonly StatInfo _statInfo;
+
+    public TablePlan(ITransaction tx, string tableName, IMetadataManager manager)
+    {
+        _tableName = tableName;
+        _tx = tx;
+        _layout = manager.GetLayout(tableName, tx);
+        _statInfo = manager.GetStatInfo(tableName, _layout, tx);
+    }
+
+    public IScan Open()
+    {
+        return new TableScan(_tx, _tableName, _layout);
+    }
+
+    public Schema Schema => _layout.Schema;
+
+    public int BlocksAccessed => _statInfo.BlocksAccessed;
+
+    public int RecordsOutput => _statInfo.RecordsOutput;
+
+    public int DistinctValues(string fieldName)
+    {
+        return _statInfo.DistinctValues(fieldName);
+    }
+}
