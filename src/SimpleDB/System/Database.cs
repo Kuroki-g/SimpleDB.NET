c65ef4398a0a69145222ba45665607c12be7aee5
Kuroki-g <64629990+Kuroki-g@users.noreply.github.com>
2025-01-16 23:45:28 +0900

    Enhance Database class to initialize file, log, buffer, and metadata managers; implement planner creation for query and update operations

--- src/SimpleDB/System/Database.cs
+++ src/SimpleDB/System/Database.cs
@@ -1,17 +1,57 @@
+using SimpleDB.DataBuffer;
+using SimpleDB.Logging;
+using SimpleDB.Metadata;
+using SimpleDB.Plan;
 using SimpleDB.Storage;
+using SimpleDB.Tx;
 
 namespace SimpleDB.System;
 
 public sealed class Database
 {
     public readonly int BlockSize;
+    private readonly IFileManager _fm;
+    private readonly ILogManager _lm;
+    private readonly IBufferManager _bm;
+    private readonly IMetadataManager _mm;
+
+    public readonly Planner Planner;
 
     public Database(ISimpleDbConfig dbConfig)
     {
         BlockSize = dbConfig.BlockSize;
 
-        var fm = new FileManager(dbConfig.FileName, BlockSize);
+        _fm = new FileManager(dbConfig.FileName, BlockSize);
+        _lm = new LogManager(_fm, dbConfig.LogFileName);
+        _bm = new BufferManager(_fm, _lm, dbConfig.BufferSize);
+
+        var tx = NewTx();
+        var isNew = _fm.IsNew;
+        if (isNew)
+        {
+            Console.WriteLine("creating new database");
+            tx.Recover();
+        }
+        else
+        {
+            Console.WriteLine("recovering existing database");
+            tx.Recover();
+        }
+        // TODO: planner switch
+        _mm = new MetadataManager(isNew, tx);
+        Planner = CreatePlanner(_mm);
+        tx.Commit();
     }
 
-    // public FileManager FileManager;
+    private static Planner CreatePlanner(IMetadataManager mm)
+    {
+        var updatePlanner = new BasicUpdatePlanner(mm);
+        var queryPlanner = new BasicQueryPlanner(mm);
+        return new Planner(queryPlanner, updatePlanner);
+    }
+
+    public ITransaction NewTx()
+    {
+        return new Transaction(_fm, _lm, _bm);
+    }
 }
