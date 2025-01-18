b2210ddef3dde1fa6e475e457f3cd6d27a4a94e5
Kuroki-g <64629990+Kuroki-g@users.noreply.github.com>
2025-01-18 23:54:59 +0900

    Add test for planner to verify plan creation with sample table

--- tests/SimpleDB.Feat.Test/Plan/PlannerTest.cs
+++ tests/SimpleDB.Feat.Test/Plan/PlannerTest.cs
@@ -1,3 +1,4 @@
+using FakeItEasy.Sdk;
 using SimpleDB.Feat.Test.Tx;
 using SimpleDB.Metadata;
 using SimpleDB.Plan;
@@ -9,11 +10,18 @@ public class PlannerTest : IntegrationTestBase
     [Fact]
     public void CreatePlan()
     {
+        {
+            CreateSampleTable("T1", CreateSchema());
+        }
         var tx = CreateTransaction();
         var mm = new MetadataManager(true, tx);
         var updatePlanner = new BasicUpdatePlanner(mm);
         var queryPlanner = new BasicQueryPlanner(mm);
         var planner = new Planner(queryPlanner, updatePlanner);
+
+        var plan = planner.CreatePlan("select B from T1 where A=10", tx);
+
+        Assert.IsType<SelectPlan>(plan);
     }
 
     [Fact]
