f2b930886c2a0f7d3ffaed46034e3de89d44afea
Kuroki-g <64629990+Kuroki-g@users.noreply.github.com>
2025-01-19 12:54:44 +0900

    Add tests for ExecuteUpdateCmd to verify insert, delete, and create table functionality

--- tests/SimpleDB.Feat.Test/Plan/PlannerTest.cs
+++ tests/SimpleDB.Feat.Test/Plan/PlannerTest.cs
@@ -1,7 +1,10 @@
+using FakeItEasy;
 using FakeItEasy.Sdk;
 using SimpleDB.Feat.Test.Tx;
 using SimpleDB.Metadata;
 using SimpleDB.Plan;
+using SimpleDB.SqlParser.Grammar.Create;
+using SimpleDB.SqlParser.Grammar.UpdateCmd;
 
 namespace SimpleDB.Feat.Test.Plan;
 
@@ -25,14 +28,47 @@ public class PlannerTest : IntegrationTestBase
     }
 
     [Fact]
-    public void ExecuteUpdateCmd()
+    public void ExecuteUpdateCmd_Insert()
     {
         var tx = CreateTransaction();
-        var mm = new MetadataManager(true, tx);
-        var updatePlanner = new BasicUpdatePlanner(mm);
-        var queryPlanner = new BasicQueryPlanner(mm);
-        var planner = new Planner(queryPlanner, updatePlanner);
+        var up = A.Fake<IUpdatePlanner>();
+        var qp = A.Fake<IQueryPlanner>();
 
-        planner.ExecuteUpdateCmd("create table test (a int)", tx);
+        var planner = new Planner(qp, up);
+
+        var cmd = "insert into T1(A,B) values(1, 'test')";
+        planner.ExecuteUpdateCmd(cmd, tx);
+
+        A.CallTo(() => up.ExecuteInsert(A<Insert>._, tx)).MustHaveHappenedOnceExactly();
+    }
+
+    [Fact]
+    public void ExecuteUpdateCmd_Delete()
+    {
+        var tx = CreateTransaction();
+        var up = A.Fake<IUpdatePlanner>();
+        var qp = A.Fake<IQueryPlanner>();
+
+        var planner = new Planner(qp, up);
+
+        var cmd = "delete from T1 where A=1";
+        planner.ExecuteUpdateCmd(cmd, tx);
+
+        A.CallTo(() => up.ExecuteDelete(A<Delete>._, tx)).MustHaveHappenedOnceExactly();
+    }
+
+    [Fact]
+    public void ExecuteUpdateCmd_CreateTable()
+    {
+        var tx = CreateTransaction();
+        var up = A.Fake<IUpdatePlanner>();
+        var qp = A.Fake<IQueryPlanner>();
+
+        var planner = new Planner(qp, up);
+
+        var cmd = "create table T2(A int, B varchar(9))";
+        planner.ExecuteUpdateCmd(cmd, tx);
+
+        A.CallTo(() => up.ExecuteCreateTable(A<CreateTable>._, tx)).MustHaveHappenedOnceExactly();
     }
 }
