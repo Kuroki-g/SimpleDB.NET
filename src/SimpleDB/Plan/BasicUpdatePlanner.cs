ca1debb0b9c0d215770c60bb69bd7de601846528
Kuroki-g <64629990+Kuroki-g@users.noreply.github.com>
2025-01-21 21:57:59 +0900

    Implement execute methods for create, delete, insert, and modify commands in BasicUpdatePlanner

--- src/SimpleDB/Plan/BasicUpdatePlanner.cs
+++ src/SimpleDB/Plan/BasicUpdatePlanner.cs
@@ -1,4 +1,5 @@
 using SimpleDB.Metadata;
+using SimpleDB.Sql;
 using SimpleDB.SqlParser.Grammar.Create;
 using SimpleDB.SqlParser.Grammar.UpdateCmd;
 using SimpleDB.Tx;
@@ -12,16 +13,72 @@ public class BasicUpdatePlanner(IMetadataManager mm) : IUpdatePlanner
     // Class implementation goes here
     public int ExecuteCreateTable(CreateTable cmd, ITransaction tx)
     {
-        throw new NotImplementedException();
+        _mm.CreateTable(cmd.TableName, cmd.NewSchema, tx);
+
+        return 0;
     }
 
+    // TODO: Implement ExecuteCreateView, ExecuteCreateIndex
+
+    /// <summary>
+    /// Execute a delete command
+    /// </summary>
+    /// <param name="cmd"></param>
+    /// <param name="tx"></param>
+    /// <returns>the number of affected records</returns>
     public int ExecuteDelete(Delete cmd, ITransaction tx)
     {
-        throw new NotImplementedException();
+        var tablePlan = new TablePlan(tx, cmd.Table, _mm);
+        var selectPlan = new SelectPlan(tablePlan, cmd.Predicate);
+        var scan = (IUpdateScan)selectPlan.Open();
+        var count = 0;
+        while (scan.Next())
+        {
+            scan.Delete();
+            count++;
+        }
+        scan.Close();
+
+        return count;
     }
 
+    /// <summary>
+    /// Execute an insert command
+    /// </summary>
+    /// <param name="cmd"></param>
+    /// <param name="tx"></param>
+    /// <returns>the number of affected records</returns>
     public int ExecuteInsert(Insert cmd, ITransaction tx)
     {
-        throw new NotImplementedException();
+        var plan = new TablePlan(tx, cmd.Table, _mm);
+        // FIXME: このキャストはおかしい
+        var scan = (IUpdateScan)plan.Open();
+        scan.Insert();
+        for (var i = 0; i < cmd.Fields.Count; i++)
+        {
+            var fld = cmd.Fields[i];
+            var value = cmd.Values[i];
+            scan.SetValue(fld, value);
+        }
+        scan.Close();
+
+        return 1;
+    }
+
+    public int ExecuteModify(Modify cmd, ITransaction tx)
+    {
+        var plan = new TablePlan(tx, cmd.TableName, _mm);
+        var selectPlan = new SelectPlan(plan, cmd.Predicate);
+        var scan = (IUpdateScan)selectPlan.Open();
+        var count = 0;
+        while (scan.Next())
+        {
+            var value = cmd.NewValue.Evaluate(scan);
+            scan.SetValue(cmd.TargetField, value);
+            count++;
+        }
+        scan.Close();
+
+        return count;
     }
 }
