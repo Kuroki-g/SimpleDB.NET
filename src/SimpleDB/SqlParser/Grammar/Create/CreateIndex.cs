130bd94b0dc68d868cdb4d286fb2fa1bae08e715
Kuroki-g <64629990+Kuroki-g@users.noreply.github.com>
2025-01-21 22:01:34 +0900

    Enhance CreateIndex and CreateView classes to include parameters for index and view definitions

--- src/SimpleDB/SqlParser/Grammar/Create/CreateIndex.cs
+++ src/SimpleDB/SqlParser/Grammar/Create/CreateIndex.cs
@@ -1,3 +1,10 @@
 namespace SimpleDB.SqlParser.Grammar.Create;
 
-public class CreateIndex() : ICreate { }
+public class CreateIndex(string indexName, string tableName, string fieldName) : ICreate
+{
+    public readonly string IndexName = indexName;
+
+    public readonly string TableName = tableName;
+
+    public readonly string FieldName = fieldName;
+}
--- src/SimpleDB/SqlParser/Grammar/Create/CreateView.cs
+++ src/SimpleDB/SqlParser/Grammar/Create/CreateView.cs
@@ -1,3 +1,10 @@
 namespace SimpleDB.SqlParser.Grammar.Create;
 
-public class CreateView() : ICreate { }
+public class CreateView(string viewName, Query query) : ICreate
+{
+    public readonly string ViewName = viewName;
+
+    public string ViewDef => _query.ToString();
+
+    private readonly Query _query = query;
+}
