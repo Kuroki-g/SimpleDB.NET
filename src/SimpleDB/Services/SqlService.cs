040b0259fce16b10d02ddc88280fea3085f2c2cb
Kuroki-g <64629990+Kuroki-g@users.noreply.github.com>
2025-01-25 18:12:03 +0900

    Log command execution and record retrieval in CreateCommand method

--- src/SimpleDB/Services/SqlService.cs
+++ src/SimpleDB/Services/SqlService.cs
@@ -11,8 +11,15 @@ public class SqlService(Database db, ILogger<SqlService> logger) : SimDbGrpc.Sql
 
     public override Task<SqlResponse> CreateCommand(SqlRequest request, ServerCallContext context)
     {
+        _logger.LogInformation("Command: {Command}", request.Command);
         var tx = _db.NewTx();
-
+        // todo: とりあえずqueryのみ実行可能にしている
+        var plan = _db.Planner.CreatePlan(request.Command, tx);
+        var scan = plan.Open();
+        while (scan.Next())
+        {
+            _logger.LogInformation("record: {Record}", scan.ToString());
+        }
         _logger.LogInformation("Received request: {Command}", request.Command);
         tx.Commit();
         return base.CreateCommand(request, context);
