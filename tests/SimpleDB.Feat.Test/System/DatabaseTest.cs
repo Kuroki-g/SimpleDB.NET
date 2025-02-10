18321a3aed20aa616512c9da8f2130d0f85241bc
Kuroki-g <64629990+Kuroki-g@users.noreply.github.com>
2025-02-10 19:17:50 +0900

    Enhance DatabaseTest with insert query validation and add tests for opening existing databases

--- tests/SimpleDB.Feat.Test/System/DatabaseTest.cs
+++ tests/SimpleDB.Feat.Test/System/DatabaseTest.cs
@@ -11,7 +11,7 @@ namespace SimpleDB.Feat.Test.System;
 public class DatabaseTest : IntegrationTestBase
 {
     [Fact]
-    public void Initialize_new_database_and_execute_some_query()
+    public void Initialize_new_database_and_execute_insert_query()
     {
         // create another directory in order to avoid conflict
         var randomStr = Helper.RandomString(12);
@@ -23,16 +23,92 @@ public class DatabaseTest : IntegrationTestBase
         var lm = new LogManager(fm, dbConfig.LogFileName);
         var bm = new BufferManager(fm, lm, dbConfig.BufferSize);
 
+        // Act
         var db = new Database(dbConfig, fm, lm, bm);
         using var tx = db.NewTx();
         var schema = CreateSchema();
         var layout = new Layout(schema);
-        var us = new TableScan(tx, "tbl", layout);
-
-        // Act
+        var ts = new TableScan(tx, "tbl", layout);
+        var insertedRecords = new List<(int, string)>();
+        for (int i = 0; i < 5; i++)
+        {
+            ts.Insert();
+            int n = (int)Math.Round((double)_random.Next() * 50);
+            ts.SetInt("A", n);
+            ts.SetString("B", "rec" + n);
+            insertedRecords.Add((n, "rec" + n));
+        }
+        tx.Commit();
+        tx.Dispose();
 
         // Assert
-        // Assert.Null(result);
+        using var tx2 = db.NewTx();
+        var ts2 = new TableScan(tx2, "tbl", layout);
+        var records = new List<(int, string)>();
+        while (ts2.Next())
+        {
+            records.Add((ts2.GetInt("A"), ts2.GetString("B")));
+        }
+
+        Assert.Equal(insertedRecords, records);
+    }
+
+    [Fact]
+    public void Initialize_created_database()
+    {
+        // create another directory in order to avoid conflict
+        var randomStr = Helper.RandomString(12);
+        var dbDir = $"./mock/{randomStr}";
+
+        // Arrange
+        var dbConfig = new SimpleDbConfig(blockSize: 4096, bufferSize: 4096, fileName: "simple.db");
+        InitializeAndStoreRecords(dbDir, dbConfig);
+
+        // Assert
+        using var fm = new FileManager(dbDir, dbConfig.BlockSize);
+        var lm = new LogManager(fm, dbConfig.LogFileName);
+        var bm = new BufferManager(fm, lm, dbConfig.BufferSize);
+
+        // Act
+        var db = new Database(dbConfig, fm, lm, bm);
+        using var tx2 = db.NewTx();
+        var schema = CreateSchema();
+        var layout = new Layout(schema);
+        var ts2 = new TableScan(tx2, "tbl", layout);
+        var records = new List<(int, string)>();
+        while (ts2.Next())
+        {
+            records.Add((ts2.GetInt("A"), ts2.GetString("B")));
+        }
+
+        // Assert.Equal(insertedRecords, records);
+    }
+
+    private void InitializeAndStoreRecords(string dbDir, SimpleDbConfig dbConfig)
+    {
+        using var fm = new FileManager(dbDir, dbConfig.BlockSize);
+        var lm = new LogManager(fm, dbConfig.LogFileName);
+        var bm = new BufferManager(fm, lm, dbConfig.BufferSize);
+
+        // Act
+        var db = new Database(dbConfig, fm, lm, bm);
+        using var tx = db.NewTx();
+        var schema = CreateSchema();
+        var layout = new Layout(schema);
+        var ts = new TableScan(tx, "tbl", layout);
+        var insertedRecords = new List<(int, string)>();
+        for (int i = 0; i < 5; i++)
+        {
+            ts.Insert();
+            int n = (int)Math.Round((double)_random.Next() * 50);
+            ts.SetInt("A", n);
+            ts.SetString("B", "rec" + n);
+            insertedRecords.Add((n, "rec" + n));
+        }
+        tx.Commit();
+        tx.Dispose();
+
+        fm.Dispose();
     }
 
     [Fact]
@@ -45,6 +121,39 @@ public class DatabaseTest : IntegrationTestBase
         // Arrange
         var dbConfig = new SimpleDbConfig(blockSize: 4096, bufferSize: 4096, fileName: "simple.db");
         var fm = new FileManager(dbDir, dbConfig.BlockSize);
+        var lm = new LogManager(fm, dbConfig.LogFileName);
+        var bm = new BufferManager(fm, lm, dbConfig.BufferSize);
+
+        var fn = new Action(() =>
+        {
+            var db = new Database(dbConfig, fm, lm, bm);
+            using var tx = db.NewTx();
+            tx.Commit();
+            tx.Dispose();
+        });
+
+        // Act
+        var result = Record.Exception(fn);
+
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public void Initialize_database_can_open_not_empty_database()
+    {
+        var dbDir = Path.Combine(_testProjectDir, "System/TestData/9HGMYKROB0DO");
+        var randomStr = Helper.RandomString(12);
+        var dbDirForTest = Directory.CreateDirectory(Path.Combine(_dir, randomStr));
+
+        foreach (var file in Directory.GetFiles(dbDir))
+        {
+            var destFile = Path.Combine(_dir, Path.GetFileName(file));
+            File.Copy(file, destFile);
+        }
+
+        // Arrange
+        var dbConfig = new SimpleDbConfig(blockSize: 4096, bufferSize: 4096, fileName: "simple.db");
+        using var fm = new FileManager(dbDirForTest.FullName, dbConfig.BlockSize);
         var lm = new LogManager(fm, dbConfig.LogFileName);
         var bm = new BufferManager(fm, lm, dbConfig.BufferSize);
 
